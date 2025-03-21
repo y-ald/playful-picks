2e13b653a6fcd4902e7a774cf9fa21b9e0ca87eb
y-ald <wpaulfernand@gmail.com>
2025-03-21 02:29:12 +0100

    [FEAT] allow lovable host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["3923f859-5de7-429d-b09a-7e8f16b14485.lovableproject.com", "localhost"],
   },
   plugins: [
     react(),
