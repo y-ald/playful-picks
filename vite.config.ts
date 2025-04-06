19d02b604fd60ec467dd33588a796e034413765d
y-ald <wpaulfernand@gmail.com>
2025-04-06 08:51:25 +0200

    [FIX] Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { splitVendorChunkPlugin } from 'vite';
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
